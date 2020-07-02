1105e8ee40e12aed432586bac325c7b938986cd4
关于 <372301467@qq.com>
2020-07-02 23:12:29 +0800

    example

---
+++ example/src/app.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .example
+    img(v-for="url in list" v-lazy-src="url")
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      list: [
+        'https://gitee.com/dggy/picpic/raw/master/demo/avatar.jpg',
+        'https://gitee.com/dggy/picpic/raw/master/demo/c.jpg',
+        'https://gitee.com/dggy/picpic/raw/master/demo/cs.jpg',
+        'https://gitee.com/dggy/picpic/raw/master/demo/pic1.jpeg',
+        'https://gitee.com/dggy/picpic/raw/master/demo/pic2.jpeg',
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+img{
+  display: block;
+  width: 400px;
+  height: 400px
+}
+</style>
---
+++ example/src/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './app.vue'
+import 'lazy-img.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
